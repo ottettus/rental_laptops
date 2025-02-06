4893cf5b43a00ca53d58981de308c0278d33821e
ottettus <etrangere767@gmail.com>
2025-02-06 18:14:11 +0100

    Add new features for client managment

--- database/db.py
+++ database/db.py
@@ -28,13 +28,43 @@ class Database:
             return result
         except psycopg2.Error as e:
             print(f"Bad fetch! Error: {e}")
-            
 
-    def update_laptop_status(self, id, new_status):
+
+    def update_laptop_status(self, id:str, new_status:str):
         query = "UPDATE laptops SET status = %s WHERE id = %s"
         try:
             self.execute_query(query, (new_status, id))
             print('Status Updated')
         except psycopg2.Error as e:
             print(f"Bad query!: Error: {e}")
+
+
+    def new_client(self, name:str, surname:str, email:str):
+        query = "INSERT INTO clients(name, surname, email) VALUES (%s, %s, %s)"
+        try:
+            self.execute_query(query,(name, surname, email))
+            print("New client addded")
+        except psycopg2.Error as e:
+            print(f"Bad fetch! Error: {e}")
+
+    
+    def delete_client(self, id:int):
+        query = "DELETE FROM clients WHERE id=%s"
+        try:
+            self.execute_query(query, (id,))
+            print(f"Client with id={id} was deleted")
+        except psycopg2.Error as e:
+            print(f"Bad fetch! Error: {e}")
+
+    
+    def get_all_clients(self):
+        query = "SELECT * FROM clients"
+        try:
+            self.fetch_query(query)
+            print(f"Client list: ")
+        except psycopg2.Error as e:
+            print(f"Bad fetch! Error: {e}")
+
+
+
 
